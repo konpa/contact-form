b4bc00d51056c8442d93d009e67a55eb06d0ecf2
Julien MONTY <julienmonty@iMac-de-Julien.local>
2014-05-19 22:32:23 +0200

    init

---
+++ app.js
@@ -0,0 +1,109 @@
+contactForm = Framer.Importer.load("imported/contact-form")
+
+/*
+ * Initialise all values
+ */
+contactForm.form.scale = 0
+contactForm.name.properties = {opacity:0, y:contactForm.name.y-10}
+contactForm.email.properties = {opacity:0, y:contactForm.email.y-10}
+contactForm.message.properties = {opacity:0, y:contactForm.message.y-10}
+contactForm.sendButton.properties = {opacity:0, y:contactForm.sendButton.y+10}
+contactForm.sending.opacity = 0
+contactForm.sent.opacity = 0
+
+/*
+ * Open form when cliking on contact button
+ */
+contactForm.button.on(Events.Click, function(event, layer) {
+
+	// Hide contact button
+	contactForm.button.visible = false
+
+	// Open empty form box
+	contactForm.form.animate({
+		properties: {scale:1.0},
+		time: 0.5,
+		curve: "cubic-bezier(0.19, 1, 0.22, 1)"
+	})
+
+	// Show all fields
+	contactForm.name.animate({
+		properties: {opacity:1.0, y:contactForm.name.y+10},
+		delay: 0.2,
+		time: 0.5,
+		curve: "cubic-bezier(0.68, -0.55, 0.265, 1.55)"
+	})
+	contactForm.email.animate({
+		properties: {opacity:1.0, y:contactForm.email.y+10},
+		delay: 0.2,
+		time: 0.5,
+		curve: "cubic-bezier(0.68, -0.55, 0.265, 1.55)"
+	})
+	contactForm.message.animate({
+		properties: {opacity:1.0, y:contactForm.message.y+10},
+		delay: 0.2,
+		time: 0.5,
+		curve: "cubic-bezier(0.68, -0.55, 0.265, 1.55)"
+	})
+	contactForm.sendButton.animate({
+		properties: {opacity:1.0, y:contactForm.sendButton.y-10},
+		delay: 0.2,
+		time: 0.5,
+		curve: "cubic-bezier(0.68, -0.55, 0.265, 1.55)"
+	})
+
+})
+
+/*
+ * Send message when cliking on "SEND MESSAGE" button
+ */
+contactForm.sendButton.on(Events.Click, function(event, layer) {
+
+	// Hide "SEND MESSAGE" button
+	contactForm.default.visible = false
+
+	contactForm.sending.animate({
+		properties: {opacity:1.0},
+		time: 0.5
+	})
+
+	// Begin sending animation (show loading circle)
+	sending = contactForm.sending.animate({
+		properties: {rotation:900},
+		time: 2
+	})
+
+	// When sending is done
+	sending.on('end', function(){
+
+		// Hide loading circle
+		contactForm.sending.animate({
+			properties: {opacity:0},
+			time: 0.3
+		})
+
+		// Show "MESSAGE SENT"
+		sent = contactForm.sent.animate({
+			properties: {opacity:1.0},
+			time: 0.5
+		})
+		
+		// When sent animation is finished
+		sent.on('end', function(){
+
+			contactForm.button.visible = true
+
+			contactForm.form.animate({
+				properties: {x:-500},
+				delay: 1,
+				time: 0.5,
+				curve: "cubic-bezier(0.68, -0.35, 0.265, 1.55)"
+			})
+
+		})
+
+	})
+
+
+
+})
---
+++ imported/contact-form/layers.json.js
@@ -0,0 +1,276 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["contact-form/layers.json.js"] = [
+	{
+		"id": 46,
+		"name": "bg",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 400,
+			"height": 700
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/bg.png",
+			"frame": {
+				"x": 0,
+				"y": 0,
+				"width": 400,
+				"height": 700
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1946194717"
+	},
+	{
+		"id": 3,
+		"name": "button",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 400,
+			"height": 700
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/button.png",
+			"frame": {
+				"x": 104,
+				"y": 319,
+				"width": 192,
+				"height": 62
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1005379002"
+	},
+	{
+		"id": 9,
+		"name": "form",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 400,
+			"height": 700
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/form.png",
+			"frame": {
+				"x": 45,
+				"y": 48,
+				"width": 311,
+				"height": 604
+			}
+		},
+		"imageType": "png",
+		"children": [
+			{
+				"id": 59,
+				"name": "closeButton",
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 400,
+					"height": 700
+				},
+				"maskFrame": null,
+				"image": {
+					"path": "images/closeButton.png",
+					"frame": {
+						"x": 330,
+						"y": 58,
+						"width": 16,
+						"height": 16
+					}
+				},
+				"imageType": "png",
+				"children": [
+					
+				],
+				"modification": "491335022"
+			},
+			{
+				"id": 18,
+				"name": "name",
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 400,
+					"height": 700
+				},
+				"maskFrame": null,
+				"image": {
+					"path": "images/name.png",
+					"frame": {
+						"x": 72,
+						"y": 95,
+						"width": 258,
+						"height": 58
+					}
+				},
+				"imageType": "png",
+				"children": [
+					
+				],
+				"modification": "1596946910"
+			},
+			{
+				"id": 24,
+				"name": "email",
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 400,
+					"height": 700
+				},
+				"maskFrame": null,
+				"image": {
+					"path": "images/email.png",
+					"frame": {
+						"x": 72,
+						"y": 165,
+						"width": 258,
+						"height": 58
+					}
+				},
+				"imageType": "png",
+				"children": [
+					
+				],
+				"modification": "1491554534"
+			},
+			{
+				"id": 28,
+				"name": "message",
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 400,
+					"height": 700
+				},
+				"maskFrame": null,
+				"image": {
+					"path": "images/message.png",
+					"frame": {
+						"x": 72,
+						"y": 235,
+						"width": 258,
+						"height": 313
+					}
+				},
+				"imageType": "png",
+				"children": [
+					
+				],
+				"modification": "2110121730"
+			},
+			{
+				"id": 30,
+				"name": "sendButton",
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 400,
+					"height": 700
+				},
+				"maskFrame": null,
+				"image": {
+					"path": "images/sendButton.png",
+					"frame": {
+						"x": 72,
+						"y": 573,
+						"width": 258,
+						"height": 55
+					}
+				},
+				"imageType": "png",
+				"children": [
+					{
+						"id": 54,
+						"name": "default",
+						"layerFrame": {
+							"x": 0,
+							"y": 0,
+							"width": 400,
+							"height": 700
+						},
+						"maskFrame": null,
+						"image": {
+							"path": "images/default.png",
+							"frame": {
+								"x": 134,
+								"y": 594,
+								"width": 134,
+								"height": 13
+							}
+						},
+						"imageType": "png",
+						"children": [
+							
+						],
+						"modification": "71433825"
+					},
+					{
+						"id": 48,
+						"name": "sending",
+						"layerFrame": {
+							"x": 0,
+							"y": 0,
+							"width": 400,
+							"height": 700
+						},
+						"maskFrame": null,
+						"image": {
+							"path": "images/sending.png",
+							"frame": {
+								"x": 187,
+								"y": 587,
+								"width": 28,
+								"height": 28
+							}
+						},
+						"imageType": "png",
+						"children": [
+							
+						],
+						"modification": "1580294546"
+					},
+					{
+						"id": 58,
+						"name": "sent",
+						"layerFrame": {
+							"x": 0,
+							"y": 0,
+							"width": 400,
+							"height": 700
+						},
+						"maskFrame": null,
+						"image": {
+							"path": "images/sent.png",
+							"frame": {
+								"x": 135,
+								"y": 594,
+								"width": 131,
+								"height": 13
+							}
+						},
+						"imageType": "png",
+						"children": [
+							
+						],
+						"modification": "1026012622"
+					}
+				],
+				"modification": "1502989244"
+			}
+		],
+		"modification": "847489574"
+	}
+]
